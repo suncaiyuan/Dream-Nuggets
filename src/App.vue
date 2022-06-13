a375b6925bebd504fe6182e342c4ccf765913a6e
suncaiyuan <1024864765@qq.com>
2022-06-13 19:55:33 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="欢迎您使用 Vue.js App"/>
   </div>
 </template>
 
